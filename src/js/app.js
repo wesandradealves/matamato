8be571aed6a2ae8b19b4c425837ea5db5e728535
Wesley Alves <wesandradealves@gmail.com>
2023-05-08 09:54:56 -0300

    - Landing page

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,27 @@
 import "../css/sass/style.scss";
+import GLightbox from 'glightbox';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 
 $(document).ready(function($){
-	console.log(true)
+	const lightbox = GLightbox({
+		touchNavigation: true,
+		loop: true,
+		autoplayVideos: true
+	});
+	$('.owl-carousel').owlCarousel({
+		mouseDrag: true,
+		touchDrag: true,
+		navSpeed: 400,
+		autoplayTimeout: 10000,
+		smartSpeed: 400,
+		dots: true,
+		items: 1,
+		autoplay: false,
+		loop: false,
+		autoplayHoverPause: true,
+		hidden: false,
+		stagePadding: 10,
+		margin:0
+	});
 });
--- webpack.common.js
+++ webpack.common.js
@@ -5,22 +5,54 @@ module.exports = {
   mode: "none",
   entry: {
     jquery: "./src/js/jquery.min.js",
-    main: "./src/js/app.js",
+    main: {
+      import: "./src/js/app.js",
+      dependOn: 'jquery',
+    },       
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),    
     new webpack.DefinePlugin({
       'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
     }),    
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    }),      
+    // new webpack.ProvidePlugin({
+    //   $: "jquery",
+    //   jQuery: "jquery"
+    // }),      
     new HTMLWebpackPlugin({
       template: "./src/index.html",
     }),
   ],
   module: {
     rules: [
+      {
+        test: /\.(css)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './'
+            }
+          }
+        ]
+      },           
+      {
+        test: /\.(gif|svg|png|avif|webp|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/'
+            }
+          }
+        ]
+      },        
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
