70dae1525dd1971f1fff1a5166b7b423d9cfa801
Wesley Alves <wesandradealves@gmail.com>
2023-05-09 02:15:58 -0300

    - mobile menu

--- jquery.js
+++ jquery.js
@@ -148,7 +148,7 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*! jQuery v3.6.
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
 /******/ 	(() => {
-/******/ 		// no baseURI
+/******/ 		__webpack_require__.b = document.baseURI || self.location.href;
 /******/ 		
 /******/ 		// object to store loaded and loading chunks
 /******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
